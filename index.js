4767874ec052e3bcdf65267a9ced4ea7be48d1d5
阳光 <392072849@qq.com>
2021-04-02 11:48:15 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-import * as Rules from './rules'
-import * as Fun from './function'
+import * as Rules from './src/rules'
+import * as Fun from './src/function'
 
 export default {
-    ...Rules,
-    ...Fun
+    install: function (Vue) {
+        Vue.prototype.$dy = {
+            fn: {},
+            rules: {}
+        };
+        Object.keys(Fun).forEach(key => { // 全局注册方法
+            Vue.prototype.$dy.fn[key] = Fun[key];
+        });
+        Object.keys(Rules).forEach(key => { // 全局注册方法
+            Vue.prototype.$dy.rules[key] = Rules[key];
+        });
+    }
 };
